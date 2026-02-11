7eacabe8a49b47dfe93b03d82de33bfc65603eac
Victor <victorcharmes.42@gmail.com>
2026-02-11 15:45:44 +0100

    Ajout autre champs, debut Crit ok

--- frontend/src/pages/machine/components/SelectionMachine.jsx
+++ frontend/src/pages/machine/components/SelectionMachine.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
 import "../style.css"
 
 function SelectionMachine({ machines = [] }) {
+  const [selectedMachine, setSelectedMachine] = useState(null)
+
+  const handleChange = (e) => {
+    const selectedName = e.target.value
+
+    const machine = machines.find(
+      (m) => m.nom === selectedName
+    )
+
+    setSelectedMachine(machine)
+  }
+
   return (
     <div className="mt-25">
       <h3 className="ml-15">Nom de la machine:</h3>
@@ -9,6 +22,7 @@ function SelectionMachine({ machines = [] }) {
         list="machinesList"
         className="border-2 rounded border-slate-900 ml-15"
         placeholder="Choisir ou saisir..."
+        onChange={handleChange}
       />
 
       <datalist id="machinesList">
@@ -16,6 +30,14 @@ function SelectionMachine({ machines = [] }) {
           <option key={machine.id} value={machine.nom} />
         ))}
       </datalist>
+
+      <h3 className="ml-15 mt-10">Criticité :</h3>
+
+      <input
+        className="border-2 rounded border-slate-900 ml-15"
+        value={selectedMachine?.criticite || ""}
+        readOnly
+      />
     </div>
   )
 }
