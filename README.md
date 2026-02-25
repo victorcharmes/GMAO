b4289fd1e9d0f5a775c8037a85dd64905740ec53
Victor <victorcharmes.42@gmail.com>
2026-02-25 13:18:15 +0100

    Mise en forme pages

---
+++ README.md
@@ -0,0 +1,5 @@
+Fonctionnalités:
+    CRUD pour gestion machines et pièces en stock
+Amélioration futures:
+    Fonctionnalités de CRUD dupliqués entre machine et stock, possible de créer des composants pour simplifier
+    Favoriser utilisation CSS vs Thymeleaf pour éviter répétitions
--- frontend/src/pages/stock/components/SupressionPiece.jsx
+++ frontend/src/pages/stock/components/SupressionPiece.jsx
@@ -1,8 +1,32 @@
+import iconeFlecheEnArriere from "../style/iconeFlecheEnArriere.svg"
+
 function SupressionPiece({setView}) {
     return(
-        <div>
-            SupressionPiece
-        </div>
+        <div className="flex gap-10">
+
+            {/* ================= COLONNE 1 ================= */}
+            <div className="w-1/3 space-y-6">
+                {/* Icônes */}
+                <div className="flex gap-4 items-center">
+                <img
+                    src={iconeFlecheEnArriere}
+                    alt="Retour"
+                    width="40"
+                    className="cursor-pointer"
+                    onClick={() => setView("selection")}
+                />
+                <h1 className="text-xl font-bold">
+                    Supression d'une pièce :
+                </h1>
+                </div>
+            </div>
+            {/* ================= COLONNE 2 ================= */}
+            <div className="w-1/3 flex flex-col items-center gap-6">
+            </div>
+            {/* ================= COLONNE 3 ================= */}
+            <div className="w-1/3">
+            </div>
+        </div>   
     )
 }
 export default SupressionPiece
