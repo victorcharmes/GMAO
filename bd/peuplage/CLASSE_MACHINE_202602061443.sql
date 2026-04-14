869a662e9ca330e35c94542031403ecad46ca426
Victor <victorcharmes.42@gmail.com>
2026-04-14 11:36:08 +0200

    Modification table classe_machine, ajout plage d'ouverture

--- bd/peuplage/CLASSE_MACHINE_202602061443.sql
+++ bd/peuplage/CLASSE_MACHINE_202602061443.sql
@@ -1,5 +1,5 @@
-INSERT INTO GMAO.CLASSE_MACHINE (CLASSE_MACHINE,DESCRIPTION_CLASSE_MACHINE) VALUES
-	 ('1E','1 équipe'),
-	 ('2E','2 équipes'),
-	 ('3E','3 équipes'),
-	 ('3E_SD','3 équipes + samedi et dimanche');
+INSERT INTO GMAO.CLASSE_MACHINE (CLASSE_MACHINE,DESCRIPTION_CLASSE_MACHINE,ouvertureDebut,ouvertureFin) VALUES
+	 ('1E','1 équipe','08:00:00','16:00:00'),
+	 ('2E','2 équipes','06:00:00','20:00:00'),
+	 ('3E','3 équipes','02:00:00','22:00:00'),
+	 ('3E_SD','3 équipes + samedi et dimanche','01:00:00','23:00:00');
